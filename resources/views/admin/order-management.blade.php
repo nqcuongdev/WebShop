aef2750208d585eff358318d904f1397475ec916
nqcuong <44604221+nqcuongdev@users.noreply.github.com>
2018-11-25 11:46:25 +0700

    Update order-management.blade.php

--- resources/views/admin/order-management.blade.php
+++ resources/views/admin/order-management.blade.php
@@ -6,31 +6,34 @@
                 <div class="row m-t-20">
                     <div class="col-md-12">
                         <h2 class="title-1 m-b-25">List Products</h2>
+                        <div class="table-data__tool">
+                            <button class="au-btn au-btn-icon au-btn--blue au-btn--small">export excel</button>
+                        </div>
                         <div class="table-responsive table-responsive-data2">
-                            <div class="table-data__tool">
-                                <button class="au-btn au-btn-icon au-btn--blue au-btn--small">export excel</button>
-                            </div>
                             <table class="table table-data2">
                                 <thead>
-                                    <tr>
+                                    <tr class="tr-shadow">
                                         <th>ID</th>
                                         <th>Name Customer</th>
                                         <th>email</th>
-                                        <th class="desc">product</th>
+                                        <th>product</th>
                                         <th>date</th>
                                         <th>price</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach($list_order as $item)
-                                        <tr>
+                                        <tr class="tr-shadow">
                                             <td>{{$item->id}}</td>
                                             <td>{{$item->name}}</td>
-                                            <td class="block-email">{{$item->email}}</td>
-                                            <td>{{$item->name_pr}}</td>
+                                            <td>
+                                                <span class="block-email">{{$item->email}}</span>
+                                            </td>
+                                            <td class="desc">{{$item->name_pr}}</td>
                                             <td>{{$item->date_order}}</td>
                                             <th>{{$item->total}}</th>
                                         </tr>
+                                        <tr class="spacer"></tr>
                                     @endforeach
                                 </tbody>
                             </table>
